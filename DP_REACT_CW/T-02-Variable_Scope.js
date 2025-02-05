c6d35d34ea572656661512da88cfe36d48be1cdc
Nipun Chamika <nipunchamika02@gmail.com>
2025-02-05 21:49:48 +0530

    DP REACT CW - Initial Commit

---
+++ DP_REACT_CW/T-02-Variable_Scope.js
@@ -0,0 +1,5 @@
+var _var = 10;
+let _let = 20;
+const _const = 30;
+
+console.log(_var, _let, _const);
---
+++ DP_REACT_CW/T-02-Variables.js
@@ -0,0 +1,10 @@
+//Three types of declaring variables
+
+//var and let - variables can change after the declaration
+var age = 25;
+let name = "Amoda";
+
+//const - variables cannot change after the declaration
+const live = "Gampha";
+
+console.log(name, age, live);
